77cb6a3fccfb2ece7bd4d9b27c5e64c7535ab596
Brandon Hilkert <brandonhilkert@gmail.com>
2012-02-18 11:23:02 -0500

    Spit out the stem words from each.

--- engtagger.rb
+++ engtagger.rb
@@ -1,4 +1,5 @@
 require 'engtagger'
+require_relative 'stemmable'
 
 tgr = EngTagger.new
 
@@ -6,4 +7,7 @@ text = "Co-founder at Meeteor. I'm a Technology Entrepreneur and have a passion 
 
 tagged = tgr.add_tags(text)
 nouns = tgr.get_nouns(tagged)
-puts nouns
+
+nouns.each do |key, value|
+  puts key.stem
+end
