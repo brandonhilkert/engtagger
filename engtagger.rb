e50e20292b1d7414cdffa3aaa92556e1c82931de
Brandon Hilkert <brandonhilkert@gmail.com>
2012-02-18 11:18:44 -0500

    Initial commit.

---
+++ engtagger.rb
@@ -0,0 +1,9 @@
+require 'engtagger'
+
+tgr = EngTagger.new
+
+text = "Co-founder at Meeteor. I'm a Technology Entrepreneur and have a passion for building something from nothing. I'm motivated by people with purpose and ambition that have a continuing drive to learn and succeed. I tend to use Ruby, Rails, MongoDB, MySQL, Heroku, EC2, Javascript, jQuery, HTML, and CSS."
+
+tagged = tgr.add_tags(text)
+nouns = tgr.get_nouns(tagged)
+puts nouns
